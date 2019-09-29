293f194b5dc68fe313fbec7b81d2cdff9d4119b4
jovanadimovska <jovana.dimovska@hotmail.com>
2019-09-29 20:24:32 +0200

    Config

---
+++ src/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class test {
+	
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("hhtp://www.google.com/");
+		
+
+	}
+
+}
