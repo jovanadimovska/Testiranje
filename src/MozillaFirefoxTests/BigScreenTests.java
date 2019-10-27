79f4f230cb380b653818fd61d4fca7d86d30c7b8
jovanadimovska <jovana.dimovska@hotmail.com>
2019-10-27 16:15:12 +0100

    added small screen and big screen tests for mozilla firefox

--- src/MozillaFirefoxTests/BigScreenTests.java
+++ src/MozillaFirefoxTests/BigScreenTests.java
@@ -21,214 +21,175 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.Keys;
 import java.util.*;
-public class BigScreenTests {
-  private WebDriver driver;
-  private Map<String, Object> vars;
-  JavascriptExecutor js;
-  @Before
-  public void setUp() {
-	System.setProperty("webdriver.gecko.driver", "Drivers\\geckodriver.exe");
-    driver = new FirefoxDriver();
-    js = (JavascriptExecutor) driver;
-    vars = new HashMap<String, Object>();
-  }
-  @After
-  public void tearDown() {
-    driver.quit();
-  }
-<<<<<<< HEAD
-  @Test
-  public void openVozenRedPage() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1552, 840));
-	    js.executeScript("window.scrollTo(0,1352)");
-	    driver.findElement(By.xpath("//*[@id=\"menu-item-3398\"]/a")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk â€“ Ð’ÐžÐ—Ð•Ð� Ð Ð•Ð”"));
-	  }
-	  
-	  @Test
-	  public void openSatbusTravel() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1552, 840));
-	    driver.findElement(By.cssSelector("#mainnav > #menu-item-3507 > a")).click();
-	    driver.findElement(By.linkText("Ð—Ð� Ð�Ð�Ð¡")).click();
-	    js.executeScript("window.scrollTo(0,140.8000030517578)");
-	    driver.findElement(By.linkText("ÐšÐžÐ�Ð¢Ð�ÐšÐ¢Ð˜Ð Ð�Ðˆ Ð�Ð•")).click();
-	    {
-	      WebElement element = driver.findElement(By.linkText("ÐšÐžÐ�Ð¢Ð�ÐšÐ¢Ð˜Ð Ð�Ðˆ Ð�Ð•"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element, 0, 0).perform();
-	    }
-	  }
-	  @Test
-	  public void openZaNas() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1325, 773));
-	    driver.findElement(By.linkText("Ð—Ð� Ð�Ð�Ð¡")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk â€“ Ð—Ð� Ð�Ð�Ð¡"));
-	  }
-	  @Test
-	  public void openKontaktirajNe() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1325, 773));
-	    driver.findElement(By.linkText("ÐšÐžÐ�Ð¢Ð�ÐšÐ¢Ð˜Ð Ð�Ðˆ Ð�Ð•")).click();
-	    driver.findElement(By.cssSelector(".dismissButton")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk â€“ ÐšÐžÐ�Ð¢Ð�ÐšÐ¢Ð˜Ð Ð�Ðˆ Ð�Ð•"));
-	  }
-	  @Test
-	  public void openPogledni() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1325, 773));
-	    driver.findElement(By.cssSelector(".large > span")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk â€“ Ð’ÐžÐ—Ð•Ð� Ð Ð•Ð”"));
-	  }
-	  @Test
-	  public void openLokalniLinii() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1326, 774));
-	    {
-	        WebElement element = driver.findElement(By.linkText("ВОЗЕН РЕД"));
-	        Actions builder = new Actions(driver);
-	        builder.moveToElement(element).perform();
-	      }
-	    driver.findElement(By.xpath("//*[@id=\"menu-item-3403\"]/a")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk – Локални линии"));
-	  }
-	  @Test
-	  public void homeSearch() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1326, 774));
-	    driver.findElement(By.cssSelector(".search-pop-button")).click();
-	    driver.findElement(By.name("s")).click();
-	    {
-	      WebElement element = driver.findElement(By.cssSelector("form > .toptip"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element, 0, 0).perform();
-	    }
-	    driver.findElement(By.name("s")).sendKeys("bosna");
-	    driver.findElement(By.cssSelector(".toptip > .fa-search")).click();
-	    assertThat(driver.findElement(By.cssSelector(".post_title > a")).getText(), is("BOSNA"));
-	  }
-	  @Test
-	  public void openMegjunarodniLinii() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1327, 775));
-	    {
-	        WebElement element = driver.findElement(By.linkText("ВОЗЕН РЕД"));
-	        Actions builder = new Actions(driver);
-	        builder.moveToElement(element).perform();
-	      }
-	    driver.findElement(By.xpath("//*[@id=\"menu-item-3399\"]/a")).click();
-	    assertThat(driver.getTitle(), is("SatBUS Mk – Meѓународни линии"));
-	  }
-	  @Test
-	  public void searchSkopje() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1550, 838));
-	    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("Скопје");
-	    assertThat(driver.findElement(By.id("B8")).getText(), is("Скопје"));
-	  }
-	  
-	  @Test
-	  public void searchLatinica() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1550, 838));
-	    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).sendKeys("Ti");
-	    assertEquals(driver.findElement(By.id(".dataTables_empty")).getText(), "Градот не постои. Пробајте повторно со кирилица.");
-	  }
-	  @Test
-	  public void searchTirana() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(1552, 840));
-	    {
-	        WebElement element = driver.findElement(By.linkText("ВОЗЕН РЕД"));
-	        Actions builder = new Actions(driver);
-	        builder.moveToElement(element).perform();
-	      }
-	    driver.findElement(By.xpath("//*[@id=\"menu-item-3399\"]/a")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).sendKeys("Тирана");
-	    assertThat(driver.findElement(By.id("B2")).getText(), is("Тирана"));
-	  }
-	
-=======
-  
-  @Test
-  public void openVozenRed() {
-    driver.get("http://satbus.mk/");
-    driver.manage().window().setSize(new Dimension(1550, 838));
-    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-    driver.findElement(By.cssSelector(".tabs-content")).click();
-    driver.findElement(By.cssSelector(".tabs-content")).click();
-    driver.findElement(By.cssSelector(".section_row:nth-child(3)")).click();
-    {
-      List<WebElement> elements = driver.findElements(By.xpath("//table[@id=\'supsystic-table-1\']/tbody/tr[2]/td"));
-      assert(elements.size() > 0);
-    }
-  }
-  
-  @Test
-  public void searchLatinica() {
-    driver.get("http://satbus.mk/");
-    driver.manage().window().setSize(new Dimension(1550, 838));
-    {
-      WebElement element = driver.findElement(By.cssSelector(".icons-social-facebook"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element).perform();
-    }
-    {
-      WebElement element = driver.findElement(By.tagName("body"));
-      Actions builder = new Actions(driver);
-      builder.moveToElement(element, 0, 0).perform();
-    }
-    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("skopje");
-    driver.findElement(By.id("supsystic-table-1_wrapper")).click();
-    driver.findElement(By.cssSelector(".dataTables_empty")).click();
-    driver.findElement(By.cssSelector(".dataTables_empty")).click();
-    assertThat(driver.findElement(By.cssSelector(".dataTables_empty")).getText(), is("Градот не постои. Пробајте повторно со кирилица."));
-  }
-  
-  @Test
-  public void searchTirana() {
-    driver.get("http://satbus.mk/");
-    driver.manage().window().setSize(new Dimension(1550, 838));
-    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).sendKeys("Тирана");
-    assertThat(driver.findElement(By.id("B2")).getText(), is("Тирана"));
-  }
 
-  @Test
-  public void searchSkopje() {
-    driver.get("http://satbus.mk/");
-    driver.manage().window().setSize(new Dimension(1550, 838));
-    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
-    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("Скопје");
-    {
-      WebElement element = driver.findElement(By.id("B3"));
-      Actions builder = new Actions(driver);
-      builder.doubleClick(element).perform();
-    }
-    assertThat(driver.findElement(By.id("B3")).getText(), is("Скопје"));
-  }
-  	
->>>>>>> a42267570215041ca8b823d46c99042ccee47936
+public class BigScreenTests {
+	private WebDriver driver;
+	private Map<String, Object> vars;
+	JavascriptExecutor js;
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.gecko.driver", "Drivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		js = (JavascriptExecutor) driver;
+		vars = new HashMap<String, Object>();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void openVozenRed() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector(".tabs-content")).click();
+		driver.findElement(By.cssSelector(".tabs-content")).click();
+		driver.findElement(By.cssSelector(".section_row:nth-child(3)")).click();
+		{
+			List<WebElement> elements = driver
+					.findElements(By.xpath("//table[@id=\'supsystic-table-1\']/tbody/tr[2]/td"));
+			assert (elements.size() > 0);
+		}
+	}
+
+	@Test
+	public void openLokalni() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		{
+			WebElement element = driver.findElement(By.cssSelector(".icons-rss"));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(element).perform();
+		}
+		{
+			WebElement element = driver.findElement(By.tagName("body"));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(element, 0, 0).perform();
+		}
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – ВОЗЕН РЕД"));
+	}
+
+	@Test
+	public void openSatbusTravel() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.cssSelector("#mainnav > #menu-item-3507 > a")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – SATBUS Travel"));
+	}
+
+	@Test
+	public void openKontakt() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("КОНТАКТИРАЈ НЕ")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – КОНТАКТИРАЈ НЕ"));
+	}
+
+	@Test
+	public void openMegjunarodni() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector(".section_row:nth-child(3)")).click();
+		driver.findElement(By.cssSelector(".tabs-content")).click();
+		{
+			List<WebElement> elements = driver.findElements(By.cssSelector(".tabs-content"));
+			assert (elements.size() > 0);
+		}
+	}
+
+	@Test
+	public void openZaNas() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("ЗА НАС")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – ЗА НАС"));
+	}
+
+	@Test
+	public void searchLatinica() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		{
+			WebElement element = driver.findElement(By.cssSelector(".icons-social-facebook"));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(element).perform();
+		}
+		{
+			WebElement element = driver.findElement(By.tagName("body"));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(element, 0, 0).perform();
+		}
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("skopje");
+		driver.findElement(By.id("supsystic-table-1_wrapper")).click();
+		driver.findElement(By.cssSelector(".dataTables_empty")).click();
+		driver.findElement(By.cssSelector(".dataTables_empty")).click();
+		assertThat(driver.findElement(By.cssSelector(".dataTables_empty")).getText(),
+				is("Градот не постои. Пробајте повторно со кирилица."));
+	}
+
+	@Test
+	public void searchSkopje() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("Скопје");
+		{
+			WebElement element = driver.findElement(By.id("B3"));
+			Actions builder = new Actions(driver);
+			builder.doubleClick(element).perform();
+		}
+		assertThat(driver.findElement(By.id("B3")).getText(), is("Скопје"));
+	}
+
+	public String waitForWindow(int timeout) {
+		try {
+			Thread.sleep(timeout);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Set<String> whNow = driver.getWindowHandles();
+		Set<String> whThen = (Set<String>) vars.get("window_handles");
+		if (whNow.size() > whThen.size()) {
+			whNow.removeAll(whThen);
+		}
+		return whNow.iterator().next();
+	}
+
+	@Test
+	public void showInvalidLink() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.cssSelector("#mainnav > #menu-item-3507 > a")).click();
+		vars.put("window_handles", driver.getWindowHandles());
+		driver.findElement(By.cssSelector(".tbutton > span")).click();
+		vars.put("win1881", waitForWindow(2000));
+		vars.put("root", driver.getWindowHandle());
+		driver.switchTo().window(vars.get("win1881").toString());
+		driver.findElement(By.cssSelector("h1")).click();
+		assertThat(driver.findElement(By.cssSelector("h1")).getText(), is("404 Not Found"));
+		driver.switchTo().window(vars.get("root").toString());
+		driver.close();
+		driver.switchTo().window(vars.get("win1881").toString());
+	}
+
+	@Test
+	public void showNotValidContact() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(1550, 838));
+		driver.findElement(By.linkText("КОНТАКТИРАЈ НЕ")).click();
+		js.executeScript("window.scrollTo(0,92)");
+		driver.findElement(By.id("sendMessage")).click();
+		assertThat(driver.findElement(By.cssSelector(".error-info")).getText(), is("Please fill out all fields."));
+		driver.findElement(By.cssSelector(".error-info")).click();
+		driver.findElement(By.cssSelector(".error-info")).click();
+	}
+
 }
--- src/MozillaFirefoxTests/SmallScreenTests.java
+++ src/MozillaFirefoxTests/SmallScreenTests.java
@@ -3,65 +3,179 @@ package MozillaFirefoxTests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
 
 public class SmallScreenTests {
-	 private WebDriver driver;
-<<<<<<< HEAD
-	 private String FIREFOX_PATH = "E:\\Jovana\\Fakultet\\treta god\\vtor semestar\\skit\\geckodriver-v0.25.0-win64\\geckodriver.exe";
-=======
-	 private String FIREFOX_PATH = "Drivers//geckodriver.exe";
->>>>>>> a42267570215041ca8b823d46c99042ccee47936
-	  JavascriptExecutor js;
-	  @Before
-	  public void setUp() {
-		System.setProperty("webdriver.gecko.driver", FIREFOX_PATH);  
-	    driver = new FirefoxDriver();
-	    js = (JavascriptExecutor) driver;
-	  }
-	  @After
-	  public void tearDown() {
-	    driver.quit();
-	  }
-	  
-<<<<<<< HEAD
-	  @Test
-	  public void vozenRedPrebaruvanje() {
-	    driver.get("http://satbus.mk/");
-	    driver.manage().window().setSize(new Dimension(550, 692));
-	    driver.findElement(By.cssSelector(".fa-bars")).click();
-	    driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
-	    driver.findElement(By.cssSelector(".grid_4:nth-child(1) span")).click();
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
-	    {
-	      WebElement element = driver.findElement(By.cssSelector(".icons-mail"));
-	      Action builder = (Action) new Actions(driver);
-	      ((Actions) builder).moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Action builder = (Action) new Actions(driver);
-	      ((Actions) builder).moveToElement(element, 0, 0).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.cssSelector(".icons-mail"));
-	      Actions builder = new Actions(driver);
-	      builder.moveToElement(element).perform();
-	    }
-	    {
-	      WebElement element = driver.findElement(By.tagName("body"));
-	      Action builder = (Action) new Actions(driver);
-	      ((Actions) builder).moveToElement(element, 0, 0).perform();
-	    }
-	    driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).sendKeys("скопје");
-	  }
-=======
->>>>>>> a42267570215041ca8b823d46c99042ccee47936
+	private WebDriver driver;
+	private Map<String, Object> vars;
+	private String FIREFOX_PATH = "Drivers//geckodriver.exe";
+	JavascriptExecutor js;
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.gecko.driver", FIREFOX_PATH);
+		driver = new FirefoxDriver();
+		js = (JavascriptExecutor) driver;
+		vars = new HashMap<String, Object>();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void openKontaktSmallTest() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(616, 701));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("КОНТАКТИРАЈ НЕ")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – КОНТАКТИРАЈ НЕ"));
+	}
+
+	@Test
+	public void openLokalni() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(688, 651));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b input")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – ВОЗЕН РЕД"));
+	}
+
+	@Test
+	public void openMegjunarodni() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(688, 651));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector(".section_row:nth-child(3)")).click();
+		driver.findElement(By.cssSelector(".tabs-content")).click();
+		{
+			List<WebElement> elements = driver.findElements(By.cssSelector(".tabs-content"));
+			assert (elements.size() > 0);
+		}
+	}
+
+	@Test
+	public void openSatBusTravel() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(589, 603));
+		driver.findElement(By.id("mobilepro")).click();
+		driver.findElement(By.cssSelector("#mainnav > #menu-item-3507 > a")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – SATBUS Travel"));
+	}
+
+	@Test
+	public void openVozenRedSmall() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(448, 701));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – ВОЗЕН РЕД"));
+	}
+
+	@Test
+	public void openZaNas() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(616, 701));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("ЗА НАС")).click();
+		assertThat(driver.getTitle(), is("SatBUS Mk – ЗА НАС"));
+	}
+
+	@Test
+	public void searchLatinica() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(880, 701));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.cssSelector(".fa-angle-down")).click();
+		driver.findElement(By.linkText("Meѓународни линии")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).click();
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).sendKeys("tirana");
+		driver.findElement(By.cssSelector("#t-1441921988-1-38cda2-ca6b238f-8e9d input")).sendKeys(Keys.ENTER);
+		driver.findElement(By.cssSelector(".dataTables_empty")).click();
+		assertThat(driver.findElement(By.cssSelector(".dataTables_empty")).getText(),
+				is("Градот не постои. Пробајте повторно со кирилица."));
+	}
+
+	public String waitForWindow(int timeout) {
+		try {
+			Thread.sleep(timeout);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Set<String> whNow = driver.getWindowHandles();
+		Set<String> whThen = (Set<String>) vars.get("window_handles");
+		if (whNow.size() > whThen.size()) {
+			whNow.removeAll(whThen);
+		}
+		return whNow.iterator().next();
+	}
+
+	@Test
+	public void searchSkopje() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(339, 701));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		{
+			WebElement element = driver.findElement(By.linkText("ВОЗЕН РЕД"));
+			Actions builder = new Actions(driver);
+			builder.moveToElement(element).perform();
+		}
+		driver.findElement(By.linkText("ВОЗЕН РЕД")).click();
+		driver.findElement(By.cssSelector(".grid_4:nth-child(1) span")).click();
+		driver.findElement(By.linkText("Петок")).click();
+		driver.findElement(By.cssSelector("#t-1441922686905-4-4cda2-ca6b input")).click();
+		driver.findElement(By.cssSelector("#t-1441922686905-4-4cda2-ca6b input")).sendKeys("скопје");
+		driver.findElement(By.cssSelector("#t-1441922686905-4-4cda2-ca6b input")).sendKeys(Keys.ENTER);
+		{
+			List<WebElement> elements = driver.findElements(By.cssSelector(".tabs-content"));
+			assert (elements.size() > 0);
+		}
+	}
+
+	@Test
+	public void showInvalidLink() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(638, 578));
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.cssSelector("#mainnav > #menu-item-3507 > a")).click();
+		vars.put("window_handles", driver.getWindowHandles());
+		driver.findElement(By.cssSelector(".tbutton > span")).click();
+		vars.put("win7357", waitForWindow(2000));
+		driver.switchTo().window(vars.get("win7357").toString());
+		assertThat(driver.getTitle(), is("404 Not Found"));
+	}
+
+	@Test
+	public void showNotValidContact() {
+		driver.get("http://satbus.mk/");
+		driver.manage().window().setSize(new Dimension(586, 653));
+		js.executeScript("window.scrollTo(0,0)");
+		driver.findElement(By.cssSelector(".fa-bars")).click();
+		driver.findElement(By.linkText("КОНТАКТИРАЈ НЕ")).click();
+		js.executeScript("window.scrollTo(0,289.6000061035156)");
+		driver.findElement(By.id("sendMessage")).click();
+		js.executeScript("window.scrollTo(0,372.79998779296875)");
+		assertThat(driver.findElement(By.cssSelector(".error-info")).getText(), is("Please fill out all fields."));
+	}
+
 }
